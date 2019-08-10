819725f4f632bea42ed6abc72c6593e7a47ac778
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2019-08-10 20:03:06 +1000

    Introduce AwesomeElixir.ScraperTest

--- config/prod.exs
+++ config/prod.exs
@@ -7,4 +7,4 @@ config :logger, level: :info
 
 deploy_on_heroku = String.to_existing_atom(System.get_env("DEPLOY_ON_HEROKU") || "false")
 
-if deploy_on_heroku, do: import_config "prod.secret.exs"
+if deploy_on_heroku, do: import_config("prod.secret.exs")
--- test/awesome_elixir/scaper_test.exs
+++ test/awesome_elixir/scaper_test.exs
@@ -2,7 +2,7 @@ defmodule AwesomeElixir.ScraperTest do
   use AwesomeElixir.DataCase
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  #  alias AwesomeElixir.Catalog.{Category, Item}
+  alias AwesomeElixir.Catalog.{Category, Item}
   alias AwesomeElixir.Scraper
 
   describe "update_index()" do
@@ -11,10 +11,18 @@ defmodule AwesomeElixir.ScraperTest do
       :ok
     end
 
-    #    test "should be store parsed data to db" do
-    #      use_cassette "grab_awesome_elixir_markdown_readme" do
-    #        Scraper.update_index()
-    #      end
-    #    end
+    test "should be store parsed data to db and schedule AwesomeElixir.Jobs.UpdateItem jobs" do
+      use_cassette "grab_awesome_elixir_markdown_readme" do
+        assert 0 == Repo.aggregate(Category, :count, :id)
+        assert 0 == Repo.aggregate(Item, :count, :id)
+        assert 0 == Repo.aggregate(from(j in "rihanna_jobs"), :count, :id)
+
+        Scraper.update_index()
+
+        assert 81 == Repo.aggregate(Category, :count, :id)
+        assert 1255 == Repo.aggregate(Item, :count, :id)
+        assert 1255 == Repo.aggregate(from(j in "rihanna_jobs"), :count, :id)
+      end
+    end
   end
 end
