a0c6efbd64e0bfe2bf09c9af8b855bd70253f407
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2019-08-17 14:43:54 +1000

    Update lib/awesome_elixir_web/templates/catalog/index.html.eex

--- lib/awesome_elixir_web/views/shared_view.ex
+++ lib/awesome_elixir_web/views/shared_view.ex
@@ -1,7 +1,6 @@
 defmodule AwesomeElixirWeb.SharedView do
   @moduledoc false
   use AwesomeElixirWeb, :view
-  use Memoize
 
-  defmemo(app_version(), do: Application.spec(:awesome_elixir, :vsn))
+  def app_version, do: Application.spec(:awesome_elixir, :vsn)
 end
