f2cd311a99fbb12a0b536b7af26a0a68517a7a8b
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2019-08-19 22:14:53 +1000

    Update README.md and fix config/dev.secret.example.exs to reflect changes

--- config/dev.secret.example.exs
+++ config/dev.secret.example.exs
@@ -4,15 +4,15 @@ config :awesome_elixir, AwesomeElixirWeb.GithubApi,
   # GitHub accept any username
   username: "awesome_elixir",
   # Replace with generated GitHub Personal Access Token
-  token: "token"
-
-config :awesome_elixir, MyApp.Guardian,
-  issuer: "awesome_elixir_app",
-  # Replace it. You can use `mix guardian.gen.secret` to get one
-  secret_key: "generated_token"
+  token: "GITHUB_TOKEN"
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   # Replace with your GitHub app Client ID
   client_id: "GITHUB_CLIENT_ID",
   # Replace with your GitHub app Client secret
   client_secret: "GITHUB_CLIENT_SECRET"
+
+config :awesome_elixir, AwesomeElixirWeb.Guardian,
+  issuer: "awesome_elixir_app",
+  # Replace it. You can use `mix guardian.gen.secret` to get one
+  secret_key: "GUARDIAN_SECRET_KEY"
